0c1ff826546948bae4afb51281b1d2b9384cb30a
Daksh Pratap Singh <73421193+Daksh4568@users.noreply.github.com>
2024-06-26 23:24:55 +0530

    Add files via upload

---
+++ 03-Objects/tempCodeRunnerFile.js
@@ -0,0 +1,10 @@
+ const course ={
+    courseName: " js in hindi" , 
+    price:"999",
+    courseInstructor:"Daksh Pratap Singh"
+ }
+
+//  console.log(course.courseInstructor)
+
+ const {courseInstructor} = course
+ console.log(courseInstructor)
